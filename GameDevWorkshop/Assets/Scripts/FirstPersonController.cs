97e7b9cc88483cdb7c6975e3471b92f7256f4d30
sorrel-kj <clevelandarg@gmail.com>
2023-09-12 10:49:20 -0400

    Character and camera controls
    Add controls for character and camera

--- GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
+++ GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
@@ -1,13 +1,52 @@
+// Sorrel Kerr-Jung 2023.09.12
+// Script for first person character controls
+
+
+// Libraries
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FirstPersonController : MonoBehaviour
 {
+
+    // Player Variables
+    public float speed = 2.0f;
+    public float gravity = -10.0f;
+    public float jumpForce = 2.0f;
+
+    // Move/Look Variables
+    private CharacterController characterController;
+    private Vector2 moveInput;
+    private Vector3 playerVelocity;
+    private bool onGround;
+    private Vector2 mouseMovement;
+
+    //Camera Variables
+    public Camera cameraLive;
+    public float sensitivity = 25.0f;
+    private float camXRotation;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void OnMove(InputAction.CallbackContext context) 
+    {
+        
+    }
+
+    public void OnLook(InputAction.CallbackContext context) 
+    {
+        
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+
     }
 
     // Update is called once per frame
